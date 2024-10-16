338b0808dbed30f30b02e9a176b876463a32d266
andrw-jns <30827518+andrw-jns@users.noreply.github.com>
2024-10-16 13:28:10 +0100

    Update name of provider sample object

--- R/lkp_tests_ec_to_aea.R
+++ R/lkp_tests_ec_to_aea.R
@@ -41,7 +41,7 @@ lkp_invst_aea <- tb_ref_invst_aea |>
 
 # 3. LOOKUP EC to AEA ------------------------------------------------
 
-lkp_tests_ec_to_aea <- pared_provider_sample |> 
+lkp_tests_ec_to_aea <- provider_sample |> 
   select(Der_EC_Investigation_All, Der_AEA_Investigation_All) |> 
   filter(str_length(Der_AEA_Investigation_All) == 2) |> 
   distinct(Der_AEA_Investigation_All, Der_EC_Investigation_All) |> 
