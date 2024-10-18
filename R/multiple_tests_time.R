49f4f35f29f048f524d7e678a06747180b8ff6f4
Steven Wyatt <57950313+swyatt-SU@users.noreply.github.com>
2024-10-18 18:08:22 +0100

    viz frequency of multiple tests

--- R/multiple_tests_time.R
+++ R/multiple_tests_time.R
@@ -63,6 +63,8 @@ df_multiple_tests_base <- pared_provider_sample |>
   mutate(id = row_number()) |>   
   select(id,
          fyear, arr_mode, acuity, acuity_desc, chief_comp_grp, inj_flag, disdest_grp,
+         dttm_arr, dttm_assess, dttm_depart,
+         dur_arr_assess, dur_arr_concl, duration_ed,
          dur_assess_concl, flag_odd_time,
          dttm_arr,
          Der_EC_Investigation_All) |> 
@@ -111,14 +113,18 @@ gc()
 
 
 # 4 visualise number of tests per patient ----
+df_multiple_tests <- readRDS(here('data', 'df_multiple_tests.RDS'))
+
 
 df_multiple_tests |> 
   group_by(fyear, n_tests_all) |> 
   summarise(n_patients = n()) |> 
   ungroup() |> 
   group_by(fyear) |> 
-  mutate(p_patients = n_patients / sum(n_patients)) |> 
+  mutate(p_patients = n_patients / sum(n_patients),
+         mean_tests = sum(n_patients * n_tests_all) / sum(n_patients)) |> 
   ggplot() +
+  geom_vline(aes(xintercept = mean_tests, colour = fyear), linetype = 'dashed') +
   geom_line(aes(x = n_tests_all, y = p_patients, colour = fyear)) +
   geom_point(aes(x = n_tests_all, y = p_patients, colour = fyear)) +
   scale_y_continuous(name = 'proportion of patients',
@@ -135,8 +141,10 @@ df_multiple_tests |>
   summarise(n_patients = n()) |> 
   ungroup() |> 
   group_by(fyear, is_admitted, inj_illness) |> 
-  mutate(p_patients = n_patients / sum(n_patients)) |> 
+  mutate(p_patients = n_patients / sum(n_patients),
+         mean_tests = sum(n_patients * n_tests_all) / sum(n_patients)) |> 
   ggplot() +
+  geom_vline(aes(xintercept = mean_tests, colour = fyear), linetype = 'dashed') +
   geom_line(aes(x = n_tests_all, y = p_patients, colour = fyear)) +
   geom_point(aes(x = n_tests_all, y = p_patients, colour = fyear)) +
   scale_y_continuous(name = 'proportion of patients',
@@ -145,7 +153,41 @@ df_multiple_tests |>
   facet_grid(cols = vars(is_admitted), rows = vars(inj_illness))
 
 
-df_multiple_tests
+df_multiple_tests |> 
+  mutate(is_admitted = ifelse(disdest_grp == "admitted", "admitted", "not admitted")) |> 
+  mutate(inj_illness = case_when(is.na(inj_flag) ~ "illness",
+                                 inj_flag == 1 ~ "injury", 
+                                 TRUE ~ "illness")) |> 
+  mutate(n_tests_all_grp = case_when(n_tests_all == 0 ~ 'no tests',
+                                     n_tests_all == 1 ~ '1 test',
+                                     n_tests_all <= 5 ~ '2-5 tests',
+                                     TRUE ~ '6+ tests')) |> 
+  mutate(n_tests_all_grp = factor(n_tests_all_grp,
+                                  levels = c('6+ tests', '2-5 tests',
+                                             '1 test', 'no tests'))) |>
+  group_by(fyear, n_tests_all_grp, is_admitted, inj_illness) |> 
+  summarise(n_patients = n()) |> 
+  ungroup() |> 
+  group_by(fyear, is_admitted, inj_illness) |> 
+  mutate(p_patients = n_patients / sum(n_patients)) |> 
+  mutate(p_patients_adj = ifelse(n_tests_all_grp == 'no tests', -p_patients, p_patients)) |> 
+  mutate(p_patients_label = paste0(as.character(round(p_patients*100, 0)), '%')) |> 
+  ggplot() +
+  geom_hline(aes(yintercept = 0), colour = 'grey') +
+  geom_col(aes(x = fyear, y = p_patients_adj, fill = n_tests_all_grp),
+           position = position_stack()) +
+  geom_text(aes(x = fyear, y = p_patients_adj, group = n_tests_all_grp, label = p_patients_label),
+           position = position_stack(vjust = 0.5)) +
+  facet_grid(cols = vars(is_admitted), rows = vars(inj_illness)) +
+  scale_fill_manual(values = c('red', 'orange', 'yellow', 'grey')) +
+  scale_y_continuous(name = 'proportion of patients',
+                     label = label_percent(accuracy = 1)) +
+  scale_x_discrete(name = 'financial year') +
+  theme(legend.title = element_blank())
+
+
+
+
 
 
 df_multiple_tests |> 
@@ -182,6 +224,162 @@ df_multiple_tests |>
   
   
 
+
+df_multiple_tests |> 
+  group_by(fyear, n_tests_Imaging) |> 
+  summarise(n_patients = n()) |> 
+  mutate(test_type = 'Imaging') |> 
+  rename(n_tests = n_tests_Imaging) |> 
+  bind_rows(df_multiple_tests |> 
+              group_by(fyear, n_tests_Haematology) |> 
+              summarise(n_patients = n()) |> 
+              mutate(test_type = 'Haematology') |> 
+              rename(n_tests = n_tests_Haematology) ) |> 
+  bind_rows(df_multiple_tests |> 
+              group_by(fyear, n_tests_Biochemistry) |> 
+              summarise(n_patients = n()) |> 
+              mutate(test_type = 'Biochemistry') |> 
+              rename(n_tests = n_tests_Biochemistry) ) |> 
+  bind_rows(df_multiple_tests |> 
+              group_by(fyear, n_tests_Other) |> 
+              summarise(n_patients = n()) |> 
+              mutate(test_type = 'Other tests') |> 
+              rename(n_tests = n_tests_Other)) |> 
+  mutate(n_tests_grp = case_when(n_tests == 0 ~ 'no tests',
+                                     n_tests == 1 ~ '1 test',
+                                     n_tests >= 1 ~ '2+ tests')) |> 
+  mutate(n_tests_grp = factor(n_tests_grp,
+                                  levels = c('2+ tests','1 test', 'no tests'))) |>
+  group_by(fyear, test_type, n_tests_grp) |> 
+  summarise(n_patients = sum(n_patients)) |> 
+  ungroup() |> 
+  group_by(fyear, test_type) |> 
+  mutate(p_patients = n_patients / sum(n_patients)) |> 
+  mutate(p_patients_adj = ifelse(n_tests_grp == 'no tests', -p_patients, p_patients)) |> 
+  mutate(p_patients_label = paste0(as.character(round(p_patients*100, 0)), '%')) |> 
+  ggplot() +
+  geom_hline(aes(yintercept = 0), colour = 'grey') +
+  geom_col(aes(x = fyear, y = p_patients_adj, fill = n_tests_grp),
+           position = position_stack()) +
+  geom_text(aes(x = fyear, y = p_patients_adj, group = n_tests_grp, label = p_patients_label),
+            position = position_stack(vjust = 0.5)) +
+  facet_wrap(vars(test_type)) +
+  scale_fill_manual(values = c('orange', 'yellow', 'grey')) +
+  scale_y_continuous(name = 'proportion of patients',
+                     #label = label_percent(accuracy = 1),
+                     breaks = c(-0.5, -0.25, 0, 0.25, 0.5),
+                     labels = c('50%', '25%', '0%', '25%', '50%')) +
+  scale_x_discrete(name = 'financial year') +
+  theme(legend.title = element_blank())
+  
+
+
+
+df_multiple_tests |> 
+  mutate(is_admitted = ifelse(disdest_grp == "admitted", "admitted", "not admitted")) |> 
+  group_by(fyear, is_admitted, n_tests_Imaging) |> 
+  summarise(n_patients = n()) |> 
+  mutate(test_type = 'Imaging') |> 
+  rename(n_tests = n_tests_Imaging) |> 
+  bind_rows(df_multiple_tests |> 
+              mutate(is_admitted = ifelse(disdest_grp == "admitted", "admitted", "not admitted")) |> 
+              group_by(fyear, is_admitted, n_tests_Haematology) |> 
+              summarise(n_patients = n()) |> 
+              mutate(test_type = 'Haematology') |> 
+              rename(n_tests = n_tests_Haematology) ) |> 
+  bind_rows(df_multiple_tests |> 
+              mutate(is_admitted = ifelse(disdest_grp == "admitted", "admitted", "not admitted")) |> 
+              group_by(fyear, is_admitted, n_tests_Biochemistry) |> 
+              summarise(n_patients = n()) |> 
+              mutate(test_type = 'Biochemistry') |> 
+              rename(n_tests = n_tests_Biochemistry) ) |> 
+  bind_rows(df_multiple_tests |> 
+              mutate(is_admitted = ifelse(disdest_grp == "admitted", "admitted", "not admitted")) |> 
+              group_by(fyear, is_admitted, n_tests_Other) |> 
+              summarise(n_patients = n()) |> 
+              mutate(test_type = 'Other tests') |> 
+              rename(n_tests = n_tests_Other)) |> 
+  mutate(n_tests_grp = case_when(n_tests == 0 ~ 'no tests',
+                                 n_tests == 1 ~ '1 test',
+                                 n_tests >= 1 ~ '2+ tests')) |> 
+  mutate(n_tests_grp = factor(n_tests_grp,
+                              levels = c('2+ tests','1 test', 'no tests'))) |>
+  group_by(fyear, is_admitted, test_type, n_tests_grp) |> 
+  summarise(n_patients = sum(n_patients)) |> 
+  ungroup() |> 
+  group_by(fyear, is_admitted, test_type) |> 
+  mutate(p_patients = n_patients / sum(n_patients)) |> 
+  mutate(p_patients_adj = ifelse(n_tests_grp == 'no tests', -p_patients, p_patients)) |> 
+  mutate(p_patients_label = paste0(as.character(round(p_patients*100, 0)), '%')) |> 
+  ggplot() +
+  geom_hline(aes(yintercept = 0), colour = 'grey') +
+  geom_col(aes(x = fyear, y = p_patients_adj, fill = n_tests_grp),
+           position = position_stack()) +
+  geom_text(aes(x = fyear, y = p_patients_adj, group = n_tests_grp, label = p_patients_label),
+            position = position_stack(vjust = 0.5)) +
+  facet_grid(rows =vars(test_type),
+             cols = vars(is_admitted)) +
+  scale_fill_manual(values = c('orange', 'yellow', 'grey')) +
+  scale_y_continuous(name = 'proportion of patients',
+                     #label = label_percent(accuracy = 1),
+                     breaks = c(-0.5, -0.25, 0, 0.25, 0.5),
+                     labels = c('50%', '25%', '0%', '25%', '50%')) +
+  scale_x_discrete(name = 'financial year') +
+  theme(legend.title = element_blank())
+
+
+
+# 5 visualise time in ED ----
+
+df_multiple_tests |> 
+  mutate(is_admitted = ifelse(disdest_grp == "admitted", "admitted", "not admitted")) |> 
+  group_by(fyear, is_admitted) |> 
+  summarise(arrival_to_assessment = mean(dur_arr_assess, na.rm = TRUE),
+            assessment_to_conclusion = mean(dur_assess_concl, na.rm = TRUE),
+            conclsuion_to_departure = mean(duration_ed - dur_arr_concl, na.rm = TRUE)) |> 
+  pivot_longer(cols = 3:5,
+               names_to = 'component_of_stay',
+               values_to = 'mean_duration') |> 
+  mutate(component_of_stay = factor(component_of_stay,
+                                    levels = c('arrival_to_assessment', 
+                                               'assessment_to_conclusion', 
+                                               'conclsuion_to_departure'))) |> 
+  ggplot() +
+  geom_col(aes(x = mean_duration, y = fyear, fill = component_of_stay),
+           position = position_stack(reverse = TRUE)) +
+  facet_wrap(vars(is_admitted)) +
+  scale_y_discrete(name = '',
+                   limits = rev)
+
+
+
+
+# 6 visualise time vs tests ----
+
+
+
+df_multiple_tests |> 
+  mutate(is_admitted = ifelse(disdest_grp == "admitted", "admitted", "not admitted")) |> 
+  mutate(n_tests = n_tests_all - n_tests_Imaging) |> 
+  group_by(fyear, n_tests, is_admitted) |> 
+  summarise(mean_duration_ed = mean(duration_ed, na.rm = TRUE),
+            mean_dur_assess_concl = mean(dur_assess_concl),
+            n_patients = n()) |> 
+  pivot_longer(cols = 4:5, 
+               names_to = 'metric',
+               values_to = 'duration') |> 
+  ggplot() +
+  geom_line(aes(x = n_tests, 
+                y = duration, 
+                colour = fyear)) +
+  geom_point(aes(x = n_tests, 
+                 y = duration, 
+                 colour = fyear,
+                 size = n_patients)) +
+  facet_grid(rows = vars(metric), cols = vars(is_admitted)) +
+  scale_y_continuous(name = 'mean duration (assessment to conclusion',
+                     limits = c(0, NA_real_)) +
+  scale_x_continuous(name = 'number of tests') 
 
 
 # df_prep_binary |>  
