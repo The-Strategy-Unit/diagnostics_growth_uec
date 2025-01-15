7b71b2244104b9639f485ac75e3e5d47ac326f05
andrw-jns <andrewghjones@gmail.com>
2025-01-15 09:36:56 +0000

    Correct cat labels in preplot trend script

--- R/plot_trends.R
+++ R/plot_trends.R
@@ -95,7 +95,7 @@ df_preplot_trends <- df_data_plus_invst |>
       group_by(fyear, procode) |>
       summarise(n_att = sum(n_att), total_invst = sum(total_invst)) |>
       ungroup() |>
-      mutate(disdest = "overall (all destinations)", .after = procode)
+      mutate(disdest = "Overall (all destinations)", .after = procode)
   ) |>
   group_by(fyear, disdest) |>
   summarise(
@@ -105,11 +105,10 @@ df_preplot_trends <- df_data_plus_invst |>
   ) |>
   ungroup() |>
   mutate(disdest = case_when(
-    disdest == "admitted" ~ "admitted patients",
-    disdest == "non-admitted" ~ "non-admitted patients",
+    disdest == "admitted" ~ "Admitted patients",
+    disdest == "non-admitted" ~ "Non-admitted patients",
     T ~ disdest
   ))
-
 
 df_preplot_trends <- df_preplot_trends |>
   arrange(disdest) |>
