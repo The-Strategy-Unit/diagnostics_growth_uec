6d06b1be385d9d0e23ee61c12b43206c100702ac
andrw-jns <andrewghjones@gmail.com>
2025-01-17 12:10:52 +0000

    Update plot trends script

--- R/plot_trends.R
+++ R/plot_trends.R
@@ -1,126 +1,28 @@
 # README
-# Plot ED test rates for sample of 28 providers (NCDR DS SERVER)
+# Plot ED test rates for sample of 28 providers 
 
-library("tidytable")
-library("DBI")
 library("here")
-library("purrr")
 library("dplyr")
+library("purrr")
 library("tidyr")
-library("dbplyr")
+library("forcats")
 library("ggplot2")
 library("stringr")
 library("janitor")
 library("lubridate")
 
+lkp_invst_ec_aea <- readRDS(here("reference", "lkp_test_ec_to_aea.rds"))
 
-con_sandbox_su <- dbConnect(
-  odbc::odbc(),
-  Driver = "SQL Server",
-  Server = "XXX",
-  Database = "NHSE_Sandbox_StrategyUnit",
-  Trusted_Connection = "True"
-)
+df_preplot_trend <- readRDS(
+  here("data", "from_ncdr_trends_241204_df_preplot.rds")
+) |> 
+  mutate(disdest = fct_relevel(disdest, "Overall (all destinations)")) |> 
+  mutate(disdest = fct_relevel(disdest, "Non-admitted patients", after = 1))
 
-tb_trend <- tbl(con_sandbox_su, in_schema("dbo", "2232_diagnostics_trend"))
-
-
-# 1. DATA MANIPULATION ------------------------------------------------------------
-
-data_provider_sample <- tb_trend |>
-  filter(procode %in% c(
-    # SAMPLE OF PROVIDERS OBTAINED FROM DQ_TRENDS QUARTO DOC:
-    "R1F",
-    "R1H",
-    "RAX",
-    "RBL",
-    "RDU",
-    "REF",
-    "RF4",
-    "RHM",
-    "RJ1",
-    "RJ2",
-    "RKB",
-    "RLQ",
-    "RM1",
-    "RNS",
-    "RP5",
-    "RPA",
-    "RQX",
-    "RR8",
-    "RRK",
-    "RTD",
-    "RTH",
-    "RTX",
-    "RWA",
-    "RWY",
-    "RX1",
-    "RXF",
-    "RXN",
-    "RYR"
-  )) |>
-  count(fyear, procode, disdest_grp, Der_Investigation_All, wt = n) |>
-  collect()
-
-# USING AEA-STYLE CODES:
-invst_codes <- c(paste0("0", 1:9), 10:23, 99)
-
-list_invst_counts <- map(invst_codes, function(invst_code) {
-  data_provider_sample %>%
-    transmute(invst = str_count(Der_Investigation_All, str_c("^", invst_code, "| ", invst_code, "|,", invst_code)))
-})
-
-df_invst_rows <- list_invst_counts |>
-  reduce(bind_cols) |>
-  clean_names() |>
-  rename(invst_99 = invst_24)
-
-df_data_plus_invst <- data_provider_sample |>
-  bind_cols(df_invst_rows) |>
-  rename(n_att = n) |>
-  # TIDYTABLE OPERATION:
-  tidytable::mutate_rowwise(n_invst = sum(c_across(starts_with("invst_")), na.rm = T)) |>
-  mutate(disdest = case_when(
-    disdest_grp == "admitted" ~ "admitted",
-    T ~ "non-admitted"
-  ), .after = procode) |>
-  mutate(total_invst = n_att * n_invst) |>
-  select(-c(starts_with("invst_"), n_invst, Der_Investigation_All, disdest_grp))
-
-###
-
-df_preplot_trends <- df_data_plus_invst |>
-  bind_rows(
-    df_data_plus_invst |>
-      group_by(fyear, procode) |>
-      summarise(n_att = sum(n_att), total_invst = sum(total_invst)) |>
-      ungroup() |>
-      mutate(disdest = "Overall (all destinations)", .after = procode)
-  ) |>
-  group_by(fyear, disdest) |>
-  summarise(
-    n_att = sum(n_att, na.rm = T),
-    n_invst = sum(total_invst, na.rm = T),
-    rate = sum(total_invst, na.rm = T) / sum(n_att, na.rm = T)
-  ) |>
-  ungroup() |>
-  mutate(disdest = case_when(
-    disdest == "admitted" ~ "Admitted patients",
-    disdest == "non-admitted" ~ "Non-admitted patients",
-    T ~ disdest
-  ))
-
-df_preplot_trends <- df_preplot_trends |> 
-  arrange(disdest) |> 
-  group_by(disdest) |> 
-  mutate(gopy_att = n_att/lag(n_att), .after = n_att) |> 
-  mutate(gopy_invst = n_invst/lag(n_invst), .after = n_invst) |> 
-  ungroup()
-  
-df_preplot_trends |> saveRDS("from_ncdr_trends_241204_df_preplot.rds")
-
-
-# 2. PLOT -----------------------------------------------------------------
+# NULL LOGIC:
+# df_preplot_trends <- readRDS(here("data", "from_ncdr_trends_250115_df_preplot.rds")) |>
+#   mutate(disdest = fct_relevel(disdest, "Overall (all destinations)")) |>
+#   mutate(disdest = fct_relevel(disdest, "Non-admitted patients", after = 1))
 
 # FOR GRAPHIC FYEAR AXIS LABEL:
 every_nth <- function(n) {
@@ -129,54 +31,65 @@ every_nth <- function(n) {
   })
 }
 
-# a. plot rate by destination --------------------------------------------------
+# 1. PLOT BY DESTINATION -----------------------------------------------------------------
 
-df_preplot_trends |>
+# df_preplot_trends |>
+#   ggplot(aes(fyear, rate)) +
+#   geom_line(aes(col = disdest, group = disdest)) +
+#   geom_point(aes(col = disdest, group = disdest)) +
+#   geom_blank(aes(y = 0)) +
+#   scale_color_manual(values =  c("#8F2D56", "dodgerblue", "#D81159")) +
+#   labs(x = "Financial year") +
+#   theme_bw() +
+#   labs(y = "Average number of recorded tests per ED attendance\n") +
+#   theme(
+#     legend.position = "none",
+#     axis.ticks = element_blank()
+#   ) +
+#   facet_wrap(vars(disdest)) +
+#   scale_x_discrete(breaks = every_nth(n = 4)) +
+#   scale_y_continuous(breaks = seq(0, 9, by = 2))
+
+df_preplot_trend |>
   ggplot(aes(fyear, rate)) +
   geom_line(aes(col = disdest, group = disdest)) +
   geom_point(aes(col = disdest, group = disdest)) +
   geom_blank(aes(y = 0)) +
-  scale_color_manual(values = c("indianred3", "dodgerblue", "purple4")) +
-  labs(x = "Financial year") +
-  theme_bw() +
-  labs(y = "Average number of recorded tests per ED attendance") +
+  scale_color_manual(values = c("#8F2D56", "dodgerblue", "#D81159")) +
+  labs(x = "Financial year")+
+  theme_bw()+
+  labs(y = "Average number of recorded tests per ED attendance\n")+
   theme(
     legend.position = "none",
     axis.ticks = element_blank()
-  ) +
-  facet_wrap(vars(disdest)) +
-  scale_x_discrete(breaks = every_nth(n = 4)) +
-  scale_y_continuous(breaks = seq(0, 9, by = 2))
+  )+
+  facet_wrap(vars(disdest))+ # , scales = "free_y"
+  scale_x_discrete(breaks = every_nth(n=4))+
+  scale_y_continuous(breaks = seq(0,9, by = 2))
 
 # b. plot rate by test type --------------------------------------------
 
-preplot_trend_by_invest <- data_provider_sample |>
-  bind_cols(df_invst_rows) |>
-  rename(n_att = n) |>
-  mutate(disdest = case_when(
-    disdest_grp == "admitted" ~ "admitted",
-    T ~ "non-admitted"
-  ), .after = procode) |>
-  mutate(across(starts_with("invst_"), ~ n_att * .)) |>
-  group_by(fyear, procode, disdest) |>
-  summarise(n_att = sum(n_att, na.rm = T), across(starts_with("invst_"), ~ sum(., na.rm = T))) |>
-  ungroup() |>
-  pivot_longer(cols = starts_with("invst_"), names_to = "invst_type", values_to = "n_invst") |>
-  group_by(fyear, disdest, invst_type) |>
-  reframe(n_att = sum(n_att, na.rm = T), n_invst = sum(n_invst, na.rm = T)) |>
-  mutate(rate = n_invst / n_att)
+preplot_trend_by_invest <- readRDS(here("data_raw", "from_ncdr_trends_241003_preplot_by_invst.rds"))
 
-preplot_trend_by_invest |> saveRDS("from_ncdr_trends_241003_preplot_by_invst.rds")
-
-options(scipen = 999)
 preplot_trend_by_invest |>
+  left_join(lkp_invst_ec_aea |> 
+              mutate(
+                # TODO NEED TO GET DESCRIPTIONS FOR LABELS
+                
+              )
+            )
+  
+  
   ggplot(aes(fyear, rate)) +
   geom_line(aes(col = disdest, group = disdest)) +
   geom_point(aes(col = disdest, group = disdest)) +
   geom_blank(aes(y = 0)) +
-  labs(x = "Financial year") +
+  labs(
+    x = "Financial year",
+    y = "Average number of recorded tests per ED attendance",
+    ) +
   theme_bw() +
-  labs(subtitle = "Vertical axis: Average number of investigations per A&E attendance") +
+  # labs(subtitle = "Vertical axis: Average number of recorded tests per ED attendance") +
   theme(
     axis.title.y = element_blank()
   ) +
