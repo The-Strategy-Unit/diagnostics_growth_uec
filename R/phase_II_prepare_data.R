42777a26a26cbc4a6dda4c94e006c4409fa4b161
andrw-jns <andrewghjones@gmail.com>
2025-03-07 15:11:46 +0000

    Tidy phase II data prep R script

--- R/phase_II_prepare_data.R
+++ R/phase_II_prepare_data.R
@@ -43,7 +43,6 @@ con_nhse_reference <- dbConnect(
   Trusted_Connection = "True"
 )
 
-
 # 1. LOAD DATA --------------------------------------------------------
 
 tb_ref_invst_ec <- tbl(con_nhse_reference, in_schema("dbo", "tbl_Ref_DataDic_ECDS_Investigation"))
@@ -55,7 +54,6 @@ lkp_invst <- tb_ref_invst_ec |>
   filter(InvestigationKey != 98) |>
   collect() |> 
   mutate(InvestigationCode = as.character(InvestigationCode))
-
 
 vec_provider_selection <- c(
   # FROM DATA QUALITY ASSESSMENT:
@@ -183,7 +181,7 @@ df_odds_fe3 <- df_odds_fe2 |>
 
 # 4. ADD OCCUPANCY VARIABLE --------------------------------------------------------
 
-# lkp_occupancy <- readRDS("lkp_occupancy.RDS")
+lkp_occupancy <- readRDS("lkp_occupancy.RDS")
 
 df_odds_occ <- df_odds_fe3 |> 
   left_join(
@@ -191,24 +189,24 @@ df_odds_occ <- df_odds_fe3 |>
     join_by(procode, month, day, hour)
   )
 
-# 5. SAMPLE 35% (OR > 1 MILLION RECORDS) ------------------------
+# 5. SAMPLE 1 MILLION RECORDS (~43%) ------------------------
 
-# set.seed(1822)
-# df_odds_sample <- df_odds_fe3 |>
-#   slice_sample(prop = 0.35)
-# 
-# gc()
-
-# # SHORTER MODEL RUN TIME FOR FASTER FEEDBACK
 set.seed(1822)
 df_odds_sample <- df_odds_occ |>
-  slice_sample(prop = 0.3)
+  slice_sample(n = 1e6)
 
 gc()
 
+
+# # SHORTER MODEL RUN TIME FOR FASTER FEEDBACK
+# set.seed(1822)
+# df_odds_sample <- df_odds_occ |>
+#   slice_sample(prop = 0.1)
+# 
+# gc()
+
 # 6. OUTCOME VARIABLES ------------------------------------------
 
-# TODO URINE CULT SITUATION
 ## a. CREATE ----------------------------------------------------
 
 # PULL EC TYPE CODES:
@@ -231,7 +229,6 @@ df_odds_invst <- list_invst_counts |>
 
 gc()
 
-
 # 7. FINAL DATA PREP ------------------------------------------------------
 
 df_prep_binary <- df_odds_invst |>
@@ -240,180 +237,11 @@ df_prep_binary <- df_odds_invst |>
   # colnames()
   # SWITCH OUTCOMES TO BINARY:
   mutate(across(starts_with("invst_"), ~ if_else(. > 0, 1, 0))) |> 
-  mutate(occ_decile = fct_relevel(as.factor(occ_decile), "5")) |> 
+  mutate(occ_decile = fct_relevel(as.factor(occ_decile), "1")) |> 
   mutate(across(c(sex, arr_mode, acuity, procode), ~ as.factor(.))) |> 
   mutate(sex = fct_relevel(sex, "m")) |> 
   mutate(acuity = fct_relevel(acuity, "1")) |> 
   mutate(arr_mode = fct_relevel(arr_mode, "walk_in")) |>
   mutate(age = as.integer(age)) 
 
-glimpse(df_prep_binary)
-
-df_prep_binary |> count((age)) |> tail()
-# ~~~~~~~~~ -------------------------------------------------------------------
-
-# 8. FUNCTIONAL PROGRAMMING SETUP (FOR MULTIPLE MODELS) ---------------------
-# FOUR MODELS RUN IN PARALLEL USING FURRR PACKAGE
-
-# (SINGLE MODEL ALSO DEMO'ED)
-
-# LOOK AT THE TOP 32 TESTS BY FREQUENCY 
-# (THERE COULD BE UP TO 4 "OTHER" GROUPS FOR LOW FREQ TESTS BASED ON THE
-# FOUR INVESTIGATION CLASSES WE MAY USE.)
-
-# CREATE DATA FRAME FOR FUNCTIONAL PROGRAMMING:
-# (TOP 10 TESTS BY APPEARANCE)
-df1 <- df_prep_binary |>
-  summarise(across(starts_with("invst_"), ~ sum(.))) |>
-  pivot_longer(cols = everything()) |>
-  arrange(-value) |>
-  mutate(id = row_number()) |> 
-  # X to Y (OF TOP Z) BY APPEARANCES:
-  slice(c(2:4, 9)) |>
-  # slice(9) |>
-  mutate(InvestigationKey = as.numeric(str_extract(name, "[:digit:]{2}"))) |>
-  left_join(lkp_invst, join_by(InvestigationKey)) |>
-  select(name, value, InvestigationDescription, id) |> 
-  # REMOVE SUPERFLUOUS TEXT:
-  mutate(InvestigationDescription = str_remove_all(InvestigationDescription, "[:punct:]")) |>
-  mutate(InvestigationDescription = str_remove_all(InvestigationDescription, " procedure")) |> 
-  relocate(InvestigationDescription, .after = name)
-
-
-# NOTE!!
-# IF YOU NEED TO PREVIEW df2 (BELOW) BEST DONE IN CONSOLE WINDOW
-# RATHER THAN WITH view()
-
-# ADD REQUIRED DATA IN LIST COLUMN (AND MANIPULATE):
-df2 <- df1 |>
-  mutate(data = list(df_prep_binary)) |>
-  mutate(data = map2(data, name, function(df, x) {
-    df |>
-      select(everything(), -matches("invst"), all_of(x)) |>
-      mutate(across(all_of(x), ~ as.factor(.))) |>
-      # TO AVOID COMPLICATION IN MODEL FORMULA:
-      rename("invst" = x)
-  }))
-
 gc()
-gc()
-
-# PREVIEW:
-df2
-
-
-# -------------------------------------------------------------------------
-
-df2$data[[1]] |> 
-  count(arr_mode)
-
-plan(multisession, workers = 4)
-
-tictoc::tic()
-df3 <- df2 |>
-  mutate(model = future_map(data, function(df) {
-    mgcv::gam(
-      formula = invst ~
-        # VAR OF INTEREST:
-        occ_decile +
-        # DEMOGRAPHICS:
-        s(age, by = sex) + sex + # imd_dec +
-        # CASE-MIX-RELATED:
-        arr_mode + acuity + chief_comp_desc + refer_sorc +
-        # TIME-RELATED:
-        is_winter + is_wkend + is_night +
-        # PROVIDER (RANDOM INTERCEPT):
-        s(procode, bs = "re"),
-      family = "binomial",
-      method = "REML",
-      data = df
-    )
-  })) 
-tictoc::toc()
-# < 1.5 hours
-# 4925.32 /60
-
-gc()
-
-df
-df3$model[[1]] |> summary()
-
-df3$model[[3]] |> 
-  broom::tidy(parametric = TRUE) |> 
-  filter(str_detect(term, "decile")) |>
-  mutate(odds = exp(estimate)) |>
-  mutate(lci = exp(estimate - 1.96*std.error)) |> 
-  mutate(uci = exp(estimate + 1.96*std.error)) |> 
-  select(term, odds, lci, uci)
-
-24*25*10
-
-# 9. SAVE MODEL RESULTS -------------------------------------------------------------
-
-df3 %>% 
-  saveRDS(str_c("models_odds_top_", min(.$id), "to", max(.$id), ".rds"))
-
-df_odds_results <- df3 |>
-  mutate(results = map(model, function(df) {
-    df |>
-      broom::tidy(parametric = TRUE) |>
-      filter(str_detect(term, "decile")) |>
-      mutate(odds = exp(estimate)) |>
-      mutate(lci = exp(estimate - 1.96*std.error)) |> 
-      mutate(uci = exp(estimate + 1.96*std.error)) |> 
-      select(term, odds, lci, uci)
-    # pull(odds)
-  })) |> 
-  select(-c(data, model, value)) |>
-  unnest(results) |>
-  # # relocate(odds, .after = pdA) |> 
-  # # relocate(std_error, .after = odds) |> 
-  # # ADJUSTMENTS IF SMALLER SAMPLE SIZE FOR MODELS 13:28 (FACTOR OR 0.35/0.17):
-  # # mutate(across(matches("^t"), ~ if_else(id %in% 13:28, . * (0.35 / 0.17), .))) %>%
-  # saveRDS(str_c("from_ncdr_growth_attrb_v4_", min(.$id), "to", max(.$id), ".rds"))
-  identity()
-
-df_odds_results
-
-
-# 10. PLOT ----------------------------------------------------------------
-
-df_odds_results |>
-  mutate(term = (str_remove_all(term, "[:alpha:]|_"))) |> 
-  mutate(term = if_else(term == 10, term, str_c(0, term))) |> 
-  # mutate(term = as.factor(term)) |> 
-  # mutate(term = fct_relevel(term, ""))
-  
-  # filter(InvestigationDescription != "Serologic test") |>
-  # mutate(invst_group = as_factor(invst_group)) |> 
-  ggplot() +
-  geom_hline(yintercept = 1, lty = "dashed", colour = "grey20", alpha = 0.5) +
-  # geom_pointrange(aes(reorder(InvestigationDescription, odds), odds, ymin = lci, ymax = uci), col = "grey40", stroke = NA)+
-  geom_pointrange(aes(term, odds, ymin = lci, ymax = uci), col = "grey40", stroke = NA)+
-  theme_bw() +
-  coord_flip() +
-  scale_y_log10(
-    limits = c(0.85, 1.15),
-    breaks = c(0.9, 1, 1.1),
-    labels = c(
-      # "Half\nas likely in\n2023/24",
-      # "Equally\nas likely in\n2023/24",
-      # "2x\nas likely in\n2023/24",
-      "90%\nas likely as\nav. occupancy",
-      "Equally\nas likely as\nav. occupancy",
-      "110%x\nas likely as\nav. occupancy"
-    )
-  )+
-  theme(
-    # axis.title.y = element_blank(),
-    axis.title = element_text(size = 9),
-    # plot.margin = margin(5, 20, 5, 5),
-    panel.grid.minor = element_blank(),
-    axis.ticks = element_blank(),
-    strip.text = element_text(size = 9)
-  ) +
-  labs(
-    x = "Occupany decile (10 = highest occupancy)",
-    y = "\nCase-mix-adjusted odds ratio of test (occupancy decile vs 'average' occ) on log scale")+
-  # facet_grid(invst_group ~ ., space = "free", scales = "free_y")
-  facet_wrap(vars(InvestigationDescription))
